da22bf8229333aa7aee15990b4f1b38c5d386a5c
Jeff Saracco <jeffsaracco@github.com>
2019-05-16 10:35:03 -0400

    UPdate

--- README.md
+++ README.md
@@ -12,4 +12,24 @@ If there is a link to the Philosophy page it follows that and is done.
 No real reason, just for fun.
 
 Usage: `ruby wiki-scraper.rb <search-term>`
-ex: `ruby wiki-scraper.rb compters`
+ex: `ruby wiki-scraper.rb seltzer`
+
+```
+1: Seltzer - Wikipedia
+2: Seltzer, Pennsylvania - Wikipedia: http://en.wikipedia.org/wiki/Seltzer,_Pennsylvania
+3: Schuylkill County, Pennsylvania - Wikipedia: http://en.wikipedia.org/wiki/Schuylkill_County,_Pennsylvania
+4: Lebanon, Pennsylvania - Wikipedia: http://en.wikipedia.org/wiki/Lebanon,_Pennsylvania
+5: Lawrence County, Pennsylvania - Wikipedia: http://en.wikipedia.org/wiki/Lawrence_County,_Pennsylvania
+6: Philadelphia - Wikipedia: http://en.wikipedia.org/wiki/Philadelphia
+7: Saunders Park, Philadelphia - Wikipedia: http://en.wikipedia.org/wiki/Saunders_Park,_Philadelphia
+8: Southwest Center City, Philadelphia - Wikipedia: http://en.wikipedia.org/wiki/Southwest_Center_City,_Philadelphia
+9: Aramingo Borough, Pennsylvania - Wikipedia: http://en.wikipedia.org/wiki/Aramingo_Borough,_Pennsylvania
+10: Philadelphia - Wikipedia: http://en.wikipedia.org/wiki/Philadelphia
+11: 1932 United States presidential election - Wikipedia: http://en.wikipedia.org/wiki/United_States_presidential_election,_1932
+12: 1888 United States presidential election - Wikipedia: http://en.wikipedia.org/wiki/1888_United_States_presidential_election
+13: Illinois - Wikipedia: http://en.wikipedia.org/wiki/Illinois
+14: Horizon League - Wikipedia: http://en.wikipedia.org/wiki/Horizon_League
+15: University of Wisconsin–Milwaukee - Wikipedia: http://en.wikipedia.org/wiki/University_of_Wisconsin%E2%80%93Milwaukee
+16: College and university rankings - Wikipedia: http://en.wikipedia.org/wiki/SCImago_Institutions_Rankings
+17: Philosophy - Wikipedia: http://en.wikipedia.org/wiki/Philosophy
+```
--- wiki-scraper.rb
+++ wiki-scraper.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-# wiki-scraper.rb by Jordan Scales
+# wiki-scraper.rb
 #
 # Tests the idea that the first link on each wikipedia article
 #   will eventually lead to philosophy
@@ -24,20 +24,23 @@ def get_page query
   @a.get query
 end
 
-def first_link page
+def first_link page, current_url
   # This was an attempt to get the actual first link but it does not ignore links in the parentheses (which causes loops)
   # href = page.search("//div[@class='mw-content-ltr']").xpath('/html/body/div[3]/div[3]/div[4]/p/a').first.attributes['href'].value
-  
+
   # links that begin with /wiki/ and are not a wikipedia info page ex. /wiki/Wikipedia:info
   links = page.links_with(:href => %r{^\/wiki\/.*}).select{|l| (l.href =~ /\/wiki\/.*:.*/) == nil}
 
   #is there a link to philosophy on the page
-  does_philosophy_exist = links.select{|l| l.href =~ /\/wiki\/Philosophy/}
+  does_philosophy_exist = links.select{|l| l.href =~ /\/wiki\/Philosophy$/}
   #if so grab it
   if(does_philosophy_exist.count > 0)
     href = does_philosophy_exist.first.href
   else
-    href = links.sample.href
+    href = current_url
+    while href == current_url
+      href = links.sample.href
+    end
   end
   "#{ROOT_URL}#{href}"
 end
@@ -47,7 +50,7 @@ end
   agent.user_agent_alias = 'Mac Safari'
 }
 
-philosophy_title = "Philosophy - Wikipedia, the free encyclopedia"
+philosophy_title = "Philosophy - Wikipedia"
 
 start = ARGV.join(' ')
 url = search_url start
@@ -57,7 +60,7 @@ puts "1: #{title}"
 count = 2
 
 while title != philosophy_title
-  url = first_link page
+  url = first_link page, url
   if !url.nil?
     page = get_page url
     title = page.title
@@ -65,6 +68,11 @@ while title != philosophy_title
     count += 1
   else
     puts "#{start} does not eventually lead to Philosophy"
+    break
+  end
+
+  if count > 1000
+    puts "#{start} does not lead to Philosophy after #{count} attempts"
     break
   end
 end
